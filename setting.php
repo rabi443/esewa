cc4f4efe6afd41459d54b059940c3c89ee3cfbfe
Rabin kumar chaudhary <rc2583463@gmail.com>
2025-01-30 16:36:53 +0545

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,8 +2,8 @@
 // Change Info From Here
 $epay_url = "https://rc.esewa.com.np/epay/main";
 $pid = "ThuloTech-9";
-$successurl = "https://brp.com.np/esewa/success.php?q=su";
-$failedurl = "https://brp.com.np/esewa/failed.php?q=fu";
+$successurl = "https://rabi443.github.io/esewa/success.php?q=su";
+$failedurl = "https://rabi443.github.io/esewa/failed.php?q=fu";
 // $merchant_code = "epay_payment"; 
 $fraudcheck_url = "https://uat.esewa.com.np/epay/transrec";
 ?>

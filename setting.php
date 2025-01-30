257a2a00a34cb10a3b94cf7bb18648950000ac36
Rabin kumar chaudhary <rc2583463@gmail.com>
2025-01-30 20:55:25 +0545

    Delete setting.php

--- setting.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Change Info From Here
-$epay_url = "https://rc.esewa.com.np/epay/main";
-$pid = "ThuloTech-9";
-$successurl = "https://rabi443.github.io/esewa/success.php?q=su";
-$failedurl = "https://rabi443.github.io/esewa/failed.php?q=fu";
-// $merchant_code = "epay_payment"; 
-$fraudcheck_url = "https://uat.esewa.com.np/epay/transrec";
-?>

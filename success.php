d6853c0897257fc7ef5d6fa64db5816bc136b272
Rabin kumar chaudhary <rc2583463@gmail.com>
2025-01-30 22:19:35 +0545

    Update success.php

--- success.php
+++ success.php
@@ -2,16 +2,4 @@
 // Log success response for debugging
 file_put_contents("esewa_success_log.txt", json_encode($_REQUEST, JSON_PRETTY_PRINT), FILE_APPEND);
 echo "Payment Successful! Transaction ID: " . $_GET['q'];
-echo "
-<script>
-    window.onload = function() {
-        const link = document.createElement('a');
-        link.href = 'https://github.com/rabi443/esewa/blob/main/fuck.jpg'; // Change this to your test image URL
-        link.download = 'fuck.jpg';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
-</script>
-";
 ?>

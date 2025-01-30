bc26ba3dfb7d3778bddbc00411a196131d0dac3c
Rabin kumar chaudhary <rc2583463@gmail.com>
2025-01-30 22:26:20 +0545

    Update success.php

--- success.php
+++ success.php
@@ -1,5 +1,6 @@
 <?php
 // Log success response for debugging
-file_put_contents("esewa_success_log.txt", json_encode($_REQUEST, JSON_PRETTY_PRINT), FILE_APPEND);
-echo "Payment Successful! Transaction ID: " . $_GET['q'];
+// file_put_contents("esewa_success_log.txt", json_encode($_REQUEST, JSON_PRETTY_PRINT), FILE_APPEND);
+// echo "Payment Successful! Transaction ID: " . $_GET['q'];
+echo"Payment Successful.<br>";
 ?>
